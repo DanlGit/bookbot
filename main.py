26ea6531d5667a87c13eb6f75773f17a7f9effce
DanlGit <thatoneorangecd@gmail.com>
2024-04-17 08:48:13 -0400

    added a character dictionary function

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 def main():
-    with open('books/frankenstein.txt') as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    char_dict = get_char_dict(text)
+    print(char_dict)
 
-if __name__ == "__main__":
-    main()
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_char_dict(text):
+    num_char_dict = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered in num_char_dict:
+            num_char_dict[lowered] += 1
+        else:
+            num_char_dict[lowered] = 1
+    return num_char_dict
+
+
+main()
 
